db0f02549d78724f4b346688eb7276eb348bc7cf
Michael Swisa <ms0504109182@gmail.com>
2024-08-21 10:16:55 +0300

    Add project files.

---
+++ Del/ManagementOfMossadAgentsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManagementOfMossadAgentsAPI.Del
+{
+    public class ManagementOfMossadAgentsDbContext : DbContext
+    {
+        public ManagementOfMossadAgentsDbContext(
+            DbContextOptions<ManagementOfMossadAgentsDbContext> options
+        )
+            : base(options)
+        {
+            Console.WriteLine(
+                "DbContext:ManagementOfMossadAgentsDbContext: " + Database.EnsureCreated()
+            );
+        }
+    }
+}
